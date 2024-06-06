5b43d5571950eacd3e2d570015fee8fba4fe62cd
khanyapupuma <khalilpupuma@gmail.com>
2024-06-06 13:55:32 +0200

    con

---
+++ script/coder.js
@@ -0,0 +1,7 @@
+document.querySelector('.submit').addEventListener('click', ()=> {
+    let input = +document.querySelector('.input').value
+    let paragraph = document.querySelector('.para')
+    paragraph.innerHTML = input * 60 
+})
+
+
